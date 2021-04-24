174fc536de349d4c8affb2dce7207fa9f3818d73
Julia Abdul-Samad <julia.abdul.samad@gmail.com>
2021-04-24 21:42:18 +0200

    Add computerMove randomNumber script.js

--- Desktop/javascript-prework/js/script.js
+++ Desktop/javascript-prework/js/script.js
@@ -1,8 +1,22 @@
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+}
+else if (randomNumber == '2') {
+  computerMove = 'papier';
+}  
+else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
 
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Mój ruch: ' + computerMove);
+
+
+
+
+
